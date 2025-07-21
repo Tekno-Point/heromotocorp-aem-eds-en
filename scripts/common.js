5b8d2bf75fbff5ce8cb833b0ddabc4f47798ae8f
Om Gupta <om.gupta@teknopoint.in>
2025-07-21 13:13:28 +0530

    log added

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,5 @@
 export const endpoint = "https://dev1.heromotocorp.com";
+console.log(endpoint);
 
 const geoLocationAPI = `${endpoint}/advancedmaps/v1/5b8424bdaf84cda4fccf61d669d85f5a/rev_geocode?lat={lat}&lng={long}`;
 const stateCityAPI = `${endpoint}/content/hero-commerce/in/en/products/product-page/practical/jcr:content.state-and-city.json`;
