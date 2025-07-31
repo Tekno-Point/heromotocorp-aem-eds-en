913ecc7932b459f7e2d0d236010b84ef70aeb82c
Om Gupta <om.gupta@teknopoint.in>
2025-07-31 18:22:41 +0530

    third party remove

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,13 +4,13 @@ setTimeout(async () => {
   openModal('/form/modals/get-a-call-back');
 }, 10000); // Delay in milliseconds
 
-// add delayed functionality here
-const scriptCode = document.createElement('script');
-scriptCode.innerHTML = `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-NJW8B6');
-`
-document.querySelector('head').append(scriptCode);
+// // add delayed functionality here
+// const scriptCode = document.createElement('script');
+// scriptCode.innerHTML = `
+// (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// 		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// 		})(window,document,'script','dataLayer','GTM-NJW8B6');
+// `
+// document.querySelector('head').append(scriptCode);
